6b6bd0e802bf336245273dd55161e79a7cde6f00
Elham Nourani <mahle68gmail.com>
2019-12-04 16:17:01 +0100

    script for theoretical investigation of delta and wind in different latitudinal zones

---
+++ delta_T_theoretical.R
@@ -0,0 +1,4 @@
+# This script investigates theoretically the hypothesis that there is higher vatiation in wind than delta T in the trade wind zone and therefore route selection (time/space)
+# should depend more on the wind, while the opposite is true in the temperate zone.
+# by: Elham Nourani. Dec, 4, 2019. Radolfzell, Germany.
+
