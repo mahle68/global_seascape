10ce6ac44a8dab23ae25f9d173cf79761bbe01d3
Elham Nourani <mahle68@pop-os.localdomain>
2019-12-17 15:18:39 +0100

    updated version. movebank file perpared

--- osprey_americas_prep.R
+++ osprey_americas_prep.R
@@ -27,6 +27,7 @@ library(parallel)
 setwd("/home/mahle68/ownCloud/Work/Projects/delta_t")
 wgs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 meters_proj <- CRS("+proj=moll +ellps=WGS84")
+mycl <- makeCluster(detectCores() - 1)
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
 
 alt_pts_temporal <- function(date_time,n_days) {
@@ -57,7 +58,9 @@ land_am_15km <- st_read("/home/mahle68/ownCloud/Work/GIS_files/ne_10m_land/ne_10
 
 save(land_am_15km,file = "R_files/land_americas_15km.RData")
 
-#### ---STEP 1: temporal filter for migratory seasons and filter for location classes (for PTTs) ####
+#### ---STEP 1: filter for adults #####
+#adults were identified based on the individual bios on http://www.ospreytrax.com/html_files/Shanawdithit.html#
+
 
 OA <- read.csv("data/Osprey_Americas/Osprey Bierregaard North and South America.csv", stringsAsFactors = F) %>% 
   dplyr::select(1,3:5,48:52) %>% #remove columns that are not needed
@@ -66,8 +69,12 @@ OA <- read.csv("data/Osprey_Americas/Osprey Bierregaard North and South America.
          track = paste(tag.local.identifier, year(date_time),sep = "_"),
          species = "Osprey",
          zone = "tradewind") %>%  #set up a season variable
-  filter(sensor.type == "gps") #keep only the gps tag data
+  filter(sensor.type == "gps",#keep only the gps tag data
+         individual.local.identifier %in% c("Holly","Hackett","Daphne","Shanawdithit","Gundersen","Wausau",
+                                            "Crabby","Charlie","Roger Tory")) #keep only adults (based on what i found on the website)
 
+
+#### ---STEP 2: spatial filter for points over the sea ####
 
 OA_sp <- OA %>% 
   drop_na(c("location.long", "location.lat")) %>% 
@@ -76,31 +83,35 @@ OA_sp <- OA %>%
 
 save(OA_sp,file = "R_files/OA_GPS_pre_filter.RData")
 
-#filter temporally.... for migration season.....................................
+load("R_files/land_americas_15km.RData") #named land_am_15km
+load("R_files/OA_GPS_pre_filter.RData")
 
-#### ---STEP 3: temporal filter for autocorrelation #####
+sea_data <- st_difference(OA_sp,land_am_15km)
+save(sea_data, file = "R_files/OA_GPS_sea.RData")
+
+#### ---STEP 3: temporal filter for migration season #####
+#assume migration is: autumn: Sep-Oct; spring: March-April...based on amount of data over water
+sea_data_migr <-sea_data %>% 
+  mutate(season = ifelse(month %in% c(3,4),"spring",
+                         ifelse (month %in% c(9,10), "autumn",
+                                 "other"))) %>% 
+  filter(season != "other")
+  
+save(sea_data_migr, file = "R_files/OA_GPS_sea_migration.RData")
+
+#### ---STEP 4: temporal filter for autocorrelation #####
+
+load("R_files/OA_GPS_sea_migration.RData") #called sea_data_migr
 
 #check for duplicated time-stamps
-getDuplicatedTimestamps(x = as.factor(OA_sp$track),timestamps = OA_sp$date_time,sensorType = "gps") #three cases of duplicated timestamps. the points are very close to each other though.
-
-
-mapview(OA_sp[c(164559:164560),])
-mapview(OA_sp[c(164561:164562),])
-mapview(OA_sp[c(164563:164564),])
-
-OA_sp<- OA_sp[-c(164559,164561,164563),] #remove the first records of duplicate timestamps;
-
+getDuplicatedTimestamps(x = as.factor(sea_data$track),timestamps = sea_data$date_time,sensorType = "gps") #three cases of duplicated timestamps. the points are very close to each other though.
 
 #thin the data to have one-hour difference?
 #create a move object
-mv <- move(x = st_coordinates(OA_sp)[,"X"],y = st_coordinates(OA_sp)[,"Y"],time = OA_sp$date_time,data = as.data.frame(OA_sp,row.names = c(1:nrow(OA_sp))),animal = OA_sp$track,proj = wgs)
-save(mv,file = "R_files/OA_GPS_pre_filter_mv.RData")
-  
-load("R_files/OA_GPS_pre_filter_mv.RData")
-  
-  #start the cluster
-mycl <- makeCluster(detectCores() - 1)
-  
+mv <- move(x = st_coordinates(sea_data)[,"X"],y = st_coordinates(sea_data)[,"Y"],time = sea_data$date_time,
+           data = as.data.frame(sea_data,row.names = c(1:nrow(sea_data))),animal = sea_data$track,proj = wgs)
+
+#start the cluster
 clusterExport(mycl, "mv") #define the variable that will be used within the function
   
 clusterEvalQ(mycl, {
@@ -125,147 +136,63 @@ sp_obj_ls <- parLapply(cl = mycl,split(mv),function(one_track){ #for each track 
 stopCluster(mycl)
   
 sp <- do.call(rbind,sp_obj_ls)
-
-
-###################################
-sea_data_15_1hr[[1]]$species <- "GFB"
-sea_data_15_1hr[[2]]$species <- "OHB"
-
-sea_data_15_1hr <- sea_data_15_1hr[[1]] %>%
-  full_join(sea_data_15_1hr[[2]], by = c("coords.x1","coords.x2","date_time","month","class","species","track")) %>% 
-  dplyr::select(c(coords.x1,coords.x2,date_time,month,class,species,track)) %>% 
-  rename(long = coords.x1, lat = coords.x2)
-
-save(sea_data_15_1hr,file = "R_files/GFB_HB_sea_data_15_1hr.RData")
+sf <- st_as_sf(sp)
 
 
 
-#### ---STEP 2: spatial filter for points over the sea ##### 
-load("R_files/land_americas_15km.RData") #named land_am_15km
-load("R_files/OA_GPS_pre_filter.RData")
+#### ---STEP 4: separate temperate and tradewind zone data #####
 
-sea_data <- st_difference(OA_sp,land_am_15km)
-save(sea_data, file = "R_files/OA_GPS_sea.RData")
+sf <-sf %>% 
+  mutate(zone = ifelse(st_coordinates(.)[,"Y"] <= 30, "tradewind",
+                       ifelse(between(st_coordinates(.)[,"Y"], 30,60), "temperate",
+         "other")))
 
-#sea_data <- lapply(OA_sp,function(x){
-#  pts_over_land <- st_interover(x,land_am_15km) #make sure the spatialpolygon layer is not a dataframe
-#  pts_over_sea <- x[is.na(pts_over_land),]
-#  pts_over_sea
-#})
+save(sf,file = "R_files/AO_sea_mgr_15_1hr.RData")
 
-#save the points
-save(sea_data,file = "R_files/GFB_HB_temp_sp_filtered.RData")
-
-
-#filter spatially with 15 km buffer
-data_sf <- lapply(data_sp,st_as_sf,crs = wgs) #convert points to sf object
-
-sea_data_15 <- lapply(data_sf,function(x){
-  pts_over_water <- st_difference(x,land_asia_15km)
-  pts_over_water
-})
-
-#save the points
-save(sea_data_15,file = "R_files/GFB_HB_temp_sp_filtered_15km.RData")
-
-#temporal and spatial filter: points over water
-load("R_files/GFB_HB_temp_sp_filtered_15km.RData") #called sea_data_15
-
-
-
-
-
-
-#temporally fitlered data for GBF and OHB
-load("R_files/GFB_HB_temp_filtered.RData") #called data_sp
-
-
-
-#### ---STEP 3: temporal filter for autocorrelation #####
-
-#check for duplicated time-stamps
-getDuplicatedTimestamps(x = as.factor(sea_data_15[[1]]$track),timestamps = sea_data_15[[1]]$date_time,sensorType = "ptt")
-getDuplicatedTimestamps(x = as.factor(sea_data_15[[2]]$track),timestamps = sea_data_15[[2]]$date_time,sensorType = "ptt") #two cases of duplicated timestamps. the points are very close to each other though.
-mapview(sea_data_15[[2]][c(111:114),])
-sea_data_15[[2]] <- sea_data_15[[2]][-c(17,112,275),] #remove the first records of duplicate timestamps; also remove the very unlikely record near india (row 275)
-
-
-#create a move list
-move_ls <- lapply(sea_data_15,function(x){
-  mv <- move(x = st_coordinates(x)[,"X"],y = st_coordinates(x)[,"Y"],time = x$date_time,data = as.data.frame(x,row.names = c(1:nrow(x))),animal = x$track,proj = wgs)
-  mv
-})
-
-#thin the data to have one-hour difference?
-sea_data_15_1hr <- lapply(move_ls,function(group){ #for each of the groups (each species) 
-  
-  sp_obj_ls <- lapply(split(group),function(one_track){ #for each track within the group
-    
-    thinned_track <- one_track %>%
-      thinTrackTime(interval = as.difftime(1, units = 'hours'), 
-                    tolerance = as.difftime(15, units = 'mins'))
-    
-    #convert back to a move object (from move burst)
-    thinned_track <- as(thinned_track,"Move")
-    thinned_track$track <- one_track@idData$track #reassign the track
-    thinned_track
-  })
-  
-  sp <- do.call(rbind,sp_obj_ls)
-  #st_as_sf(sp) #conver the spatialdataframe to an sf object
-  as.data.frame(sp)
-}) 
-
-sea_data_15_1hr[[1]]$species <- "GFB"
-sea_data_15_1hr[[2]]$species <- "OHB"
-
-sea_data_15_1hr <- sea_data_15_1hr[[1]] %>%
-  full_join(sea_data_15_1hr[[2]], by = c("coords.x1","coords.x2","date_time","month","class","species","track")) %>% 
-  dplyr::select(c(coords.x1,coords.x2,date_time,month,class,species,track)) %>% 
-  rename(long = coords.x1, lat = coords.x2)
-
-save(sea_data_15_1hr,file = "R_files/GFB_HB_sea_data_15_1hr.RData")
-
-
-#### ---STEP 4: produce alternative points in time #####
+#### ---STEP 5: produce alternative points in time #####
 
 #open data
-load("R_files/GFB_HB_sea_data_15_1hr.RData") #called sea_data_15_1hr
-#load("R_files/GFB_HB_temp_sp_filtered_15km_ann.RData") #called ann_df ...this is from STEP 4 of the previous version. just combine the two datasets without annotation
+load("R_files/AO_sea_mgr_15_1hr.RData") #called sf
 
 #for each point, create a alternative points a week before and a week after the observed point. year and hour dont change.
-alts <- sea_data_15_1hr %>%
+alts <- sf %>%
   mutate(#date_time = as.POSIXct(strptime(date_time,format = "%Y-%m-%d %H:%M:%S"),tz = "UTC"),
          obs_id = row_number()) %>%
   slice(rep(row_number(),15)) %>% #copy each row 60 times. 1 used, 60 alternative
   arrange(date_time) %>%
   mutate(used = ifelse(row_number() == 1,1,
-                       ifelse((row_number() - 1) %% 15 == 0, 1, 0))) #assign used and available values
+                       ifelse((row_number() - 1) %% 15 == 0, 1, 0)),  #assign used and available values
+         lon = st_coordinates(.)[,"X"],
+         lat = st_coordinates(.)[,"Y"]) %>% 
+  st_drop_geometry()%>% 
+  filter(individual.local.identifier %in% c("Holly","Hackett","Daphne","Shanawdithit","Gundersen","Wausau",
+                                            "Crabby","Charlie","Roger Tory"))
   
 
-  alt_ls <- lapply( split(alts,alts$obs_id),function(x){ #didnt manage to write this part using dplyr and purrr
+alt_ls <- lapply( split(alts,alts$obs_id),function(x){ #didnt manage to write this part using dplyr and purrr
     alt_times <- alt_pts_temporal(x$date_time[1],14)
     x %>%
       mutate(timestamp = as.POSIXct(strptime(c(as.character(x$date_time[1]),alt_times$dt),format = "%Y-%m-%d %H:%M:%S"),tz = "UTC"),
       period = c("now",alt_times$period))
-  })
+})
 
-  alt_cmpl <- do.call(rbind,alt_ls)
-  
-save(alt_cmpl,file = "R_files/GFB_HB_temp_sp_filtered_15km_alt_14days.RData")
-  
-#### ---STEP 5: annotate alternative points with delta T #####
+alt_cmpl <- do.call(rbind,alt_ls)
 
-load("R_files/GFB_HB_temp_sp_filtered_15km_alt_14days.RData") #called ann_df_alt_cmpl
+  
+save(alt_cmpl,file = "R_files/AO_temp_sp_filtered_15km_alt_14days.RData")
+  
+#### ---STEP 6: annotate alternative points with delta T #####
+
+load("R_files/AO_temp_sp_filtered_15km_alt_14days.RData") #called ann_df_alt_cmpl
 
 #prep for track annotation on movebank
 alt_cmpl_mb <- alt_cmpl %>%
-  mutate(timestamp = paste(as.character(timestamp),"000",sep = ".")) 
+  mutate(timestamp = paste(as.character(timestamp),"000",sep = "."))
   
 #rename columns
-colnames(alt_cmpl_mb)[c(1,2)] <- c("location-long","location-lat")
+colnames(alt_cmpl_mb)[c(11,12)] <- c("location-long","location-lat")
 
-write.csv(alt_cmpl_mb,"R_files/GFB_HB_temp_sp_filtered_15km_alt_14days.csv")
+write.csv(alt_cmpl_mb,"R_files/AO_temp_sp_filtered_15km_alt_14days.csv")
   
 #downloaded from movebank
 ann <- read.csv("movebank_annotation/GFB_HB_temp_sp_filtered_15km_alt_14days.csv-799772115122349617.csv", stringsAsFactors = F) %>%
@@ -279,7 +206,7 @@ ann <- read.csv("movebank_annotation/GFB_HB_temp_sp_filtered_15km_alt_14days.csv
 save(ann, file = "R_files/GFB_HB_temp_sp_filtered_15km_alt_ann_14days.RData")
 
 
-#### ---STEP 6: visualizations #####
+#### ---STEP 7: visualizations #####
   
 load("R_files/GFB_HB_temp_sp_filtered_15km_alt_ann_14days.RData") #called ann
 
@@ -307,7 +234,7 @@ lines(density(ann[ann$period != "now","v925"]),col = "green")
 } #not much seasonal difference. there seems to be selection for u-wind
 
   
-#### ---STEP 7: analysis #####
+#### ---STEP 8: analysis #####
 
 #standardize the variables to have comparable effect sizes
 ann_sc <- ann %>%
@@ -327,7 +254,7 @@ model_cl
         model_cl_b <- stan_clogit(used ~ delta_t + u925 + v925, strata = obs_id, data = ann_sc)
 
 
-#### ---STEP 8: modeling #####
+#### ---STEP 9: modeling #####
 #model only for autumn
 autumn_one_week_before <- ann %>%
   filter(period %in% c("now","before") & week %in% c("obs_day", "week_one") & season == "autumn" ) %>%
@@ -371,6 +298,7 @@ model
 #evaluate the model
 
 
+
 ####---PLOTTINTG #####
 windows()
 plot(land_asia)
@@ -394,4 +322,9 @@ points(ann[is.na(ann$delta_T),c("location.long","location.lat")])
 
 
 lapply(sea_data_15,mapview)
+
+
+plot(land_am_15km)
+points(sp,cex = 0.3, col = factor(sp$month))
+
 #### end ####
