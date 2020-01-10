fbc67c5a1f00661fafe85aa972f957bd86866789
mahle68 <mahle68@gmail.com>
2020-01-10 15:18:34 +0100

    updated version

--- all_data_clogit_permutation.R
+++ all_data_clogit_permutation.R
@@ -1,6 +1,7 @@
-#scripts for meta-analysis of sea-crossing behavior
+#scripts for meta-analysis of sea-crossing behavior using permutation tests
 #follows the data preparation in all_data_prep.R
-#Jan 3. 2020. Elham Nourani. Radolfzell, Germany.
+#Jan 10. 2020. Elham Nourani. Radolfzell, Germany.
+
 
 library(dplyr)
 library(lme4)
@@ -21,17 +22,58 @@ wgs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 meters_proj <- CRS("+proj=moll +ellps=WGS84")
 mycl <- makeCluster(detectCores() - 2)
 
-##### STEP 1: check the data #####
+setwd("/home/enourani/ownCloud/Work/Projects/delta_t")
 
+
+#Question: is the effect of delta T larger than wind in each zone?
+
+
+##### STEP 1: calculate the observed statistic #####
 load("R_files/all_spp_temp_sp_filtered_15km_alt_ann_14days.RData") #called ann
 
-lapply(split(ann,ann$zone), function(x){
-  X11()
-  par(mfrow = c(1,3))
-  hist(ann[ann])
+#scale the variables, zone-specific
+ann_z<-ann%>%
+  group_by(zone) %>% #cosider season later 
+  mutate_at(c(21,17,14),
+            list(z=~scale(.)))%>%
+  as.data.frame()
+
+formula <- used ~ delta_t_z + u925_z + v925_z + strata(obs_id)
+model_twz_obs <- clogit(formula, data = ann_z[ann_z$zone == "tradewind",])
+summary(model_twz_obs)
+
+coef(model_twz_obs)
+stat_obs_twz_u <- abs(coef(model_twz_obs)[1])-abs(coef(model_twz_obs)[2])
+stat_obs_twz_v <- abs(coef(model_twz_obs)[1])-abs(coef(model_twz_obs)[3])
+
+##### STEP 2: create alternative datasets #####
+permutations <- 1000
+
+#create alternative datasets  
+new_data_twz_ls <- lapply(1:permutations, function(x){ #for each permutation
+  new_obs_id_ls <-lapply(split(ann_z[ann_z$zone == "tradewind",], ann_z[ann_z$zone == "tradewind","obs_id"]), function(y){ #for each obs_id
+    rnd_obs <- sample(1:15,1) #draw a random number to be the new index for used row
+    y[rnd_obs,"used"] <- 1
+    y[-rnd_obs,"used"] <- 0
+    y
+  })
+  new_obs_id <- do.call(rbind, new_obs_id_ls)
+  new_obs_id
+}) 
+
+##### STEP 3: calculate the random statistics #####
+
+rnd_st_twz_ls <- lapply(new_data_twz_ls, function(x){
+  model <-  clogit(formula, data = x[x$zone == "tradewind",])
+  stat_twz_u <- abs(coef(model_twz_obs)[1])-abs(coef(model_twz_obs)[2])
+  stat_twz_v <- abs(coef(model_twz_obs)[1])-abs(coef(model_twz_obs)[3])
+  data.frame(u = stat_twz_u, v= stat_twz_v, row.names = "")
 })
 
-#correlation
+rnd_stat_twz <- do.call(rbind, rnd_st_twz_ls)
+
+
+##### STEP 4: plot the random and observed values #####
 
 
 ##### STEP 2: modeling... play around with options #####
@@ -43,7 +85,7 @@ ann_z<-ann%>%
             list(z=~scale(.)))%>%
   as.data.frame()
 
-
+  
 #formula1 <- used ~ scale(delta_t) + scale(u925) + scale(v925) + strata(obs_id)
 formula1_sc <- used ~ delta_t_z + u925_z + v925_z + strata(obs_id)
 
