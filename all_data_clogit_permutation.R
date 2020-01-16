40305579f74231d52b8dc3f87b40f341b2723caf
mahle68 <mahle68@gmail.com>
2020-01-16 11:13:06 +0100

    updated version

--- all_data_clogit_permutation.R
+++ all_data_clogit_permutation.R
@@ -101,69 +101,60 @@ p_value_v <- sum(stat_obs_twz_v <= rnd_stat_twz$v) / permutations # 2: hypothesi
 
 
 
-################################################################ temperate zone
+################################################################ season- and zone- specific
 #Question: is the effect of delta T larger than wind in each zone?
 
 ##### STEP 1: calculate the observed statistic #####
 
-model_tmpz_obs <- clogit(formula, data = ann_z[ann_z$zone == "temperate",])
-summary(model_tmpz_obs) 
-AIC(model_tmpz_obs) #1759.656
+for(i in c("spring","autumn")){
+  for(j in c("temperate", "tradewind")){
+    
+    data <- ann_z[ann_z$zone == j & ann_z$season == i,]
+      
+    model_obs <- clogit(formula, data = data)
 
-model_tmpz_obs2 <- clogit(formula2, data = ann_z[ann_z$zone == "temperate",])
-summary(model_tmpz_obs2)
-AIC(model_tmpz_obs2) #1758.918
+    obs_stat_u <- abs(coef(model_obs)[1])-abs(coef(model_obs)[2])
+    obs_stat_v <- abs(coef(model_obs)[1])-abs(coef(model_obs)[3])
+    
+    ##### STEP 2: create alternative datasets #####
+    permutations <- 1000
+    
+    rnd_stat_df <- lapply(1:permutations, function(x){ #for each permutation
+      #create alternative datasets  
+      rnd_obs_id<-lapply(split(data, data$obs_id), function(y){ #for each obs_id
+        rnd_obs <- sample(1:15,1) #draw a random number to be the new index for used row
+        y[rnd_obs,"used"] <- 1
+        y[-rnd_obs,"used"] <- 0
+        y
+      }) %>% 
+        reduce(rbind)
+      #calcuate randomized stat
+      model <-  clogit(formula, data = rnd_obs_id)
+      rnd_stat_u <- abs(coef(model)[1])-abs(coef(model)[2])
+      rnd_stat_v <- abs(coef(model)[1])-abs(coef(model)[3])
+      data.frame(u = rnd_stat_u, v= rnd_stat_v, row.names = "")
+      
+    }) %>% 
+      reduce(rbind)
 
-coef(model_tmpz_obs)
-stat_obs_tmpz_u <- abs(coef(model_tmpz_obs)[1])-abs(coef(model_tmpz_obs)[2])
-stat_obs_tmpz_v <- abs(coef(model_tmpz_obs)[1])-abs(coef(model_tmpz_obs)[3])
+    ##### STEP 3: plot the random and observed values #####
 
-##### STEP 2: create alternative datasets #####
-permutations <- 1000
-
-#create alternative datasets  
-new_data_tmpz_ls <- lapply(1:permutations, function(x){ #for each permutation
-  new_obs_id_ls <-lapply(split(ann_z[ann_z$zone == "temperate",], ann_z[ann_z$zone == "temperate","obs_id"]), function(y){ #for each obs_id
-    rnd_obs <- sample(1:15,1) #draw a random number to be the new index for used row
-    y[rnd_obs,"used"] <- 1
-    y[-rnd_obs,"used"] <- 0
-    y
-  })
-  new_obs_id <- do.call(rbind, new_obs_id_ls)
-  new_obs_id
-}) 
-
-##### STEP 3: calculate the random statistics #####
-
-rnd_st_tmpz_ls <- lapply(new_data_tmpz_ls, function(x){
-  model <-  clogit(formula, data = x[x$zone == "temperate",])
-  stat_tmpz_u <- abs(coef(model)[1])-abs(coef(model)[2])
-  stat_tmpz_v <- abs(coef(model)[1])-abs(coef(model)[3])
-  data.frame(u = stat_tmpz_u, v= stat_tmpz_v, row.names = "")
-})
-
-rnd_stat_tmpz <- do.call(rbind, rnd_st_tmpz_ls)
-
-
-##### STEP 4: plot the random and observed values #####
-plot(1:permutations, rnd_stat_tmpz$u, type = "l", main = "temperate - u wind")
-abline(h = stat_obs_tmpz_u, col = "red")
-
-par(mfrow = c(1,2))
-#plot the frequency distribution of u vlaues
-hist(rnd_stat_tmpz$u, breaks = 50, col = "lightgrey", xlim = c(-0.3,0.3), main = "delta_t - wind_u")
-abline(v = stat_obs_tmpz_u, col = "red") #random network is homogenous, that is why cv goes down the more randomization we do
-
-#plot the frequency distribution of v vlaues
-hist(rnd_stat_tmpz$v, breaks = 50, col = "lightgrey", xlim = c(-0.3,0.3), main = "delta_t - wind_v")
-abline(v = stat_obs_tmpz_v, col = "red") #random network is homogenous, that is why cv goes down the more randomization we do
-
-
-##### STEP 5: calculate p-values #####
-#calculate the p-value. one-tailed
-p_value_u <- sum(stat_obs_tmpz_u <= rnd_stat_tmpz$u) / permutations # 1: hypothesis is rejected. delta t does not have a higher effect than u wind
-p_value_v <- sum(stat_obs_tmpz_v <= rnd_stat_tmpz$v) / permutations # 2: hypothesis is accepted. delta t has a higher effect than v wind
-
-
+    par(mfrow = c(1,2))
+    #plot the frequency distribution of u vlaues
+    hist(rnd_stat_df$u, breaks = 50, xlim = c(-0.33, 0.35),col = "lightgrey",main = "delta_t - wind_u", xlab = "wind_u")
+    abline(v = obs_stat_u, col = "red") #random network is homogenous, that is why cv goes down the more randomization we do
+    
+    #plot the frequency distribution of v vlaues
+    hist(rnd_stat_df$v, breaks = 50, col = "lightgrey", main = "delta_t - wind_v", xlab = "wind_v")
+    abline(v = obs_stat_v, col = "red") #random network is homogenous, that is why cv goes down the more randomization we do
+    
+    
+    ##### STEP 4: calculate p-values #####
+    #calculate the p-value. one-tailed
+    p_value_u <- sum(obs_stat_u <= rnd_stat_df$u) / permutations # 1: hypothesis is accepted. delta t does has a higher effect than u wind
+    p_value_v <- sum(obs_stat_v <= rnd_stat_df$v) / permutations # 2: hypothesis is accepted. delta t has a higher effect than v wind
+    
+  }
+}
 
 
