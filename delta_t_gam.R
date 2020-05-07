189d6b5ebe66a083a5f7cf8c914bfd5f307c8548
mahle68 <mahle68@gmail.com>
2020-05-07 13:02:41 +0200

    updated version

--- delta_t_gam.R
+++ delta_t_gam.R
@@ -1,7 +1,7 @@
 #scripts for building a global GAM for delta-t
 #data processed in R_files/global_seascape.R
 #May 6, 2020. Radolfzell, DE. Elham Nourani, PhD
-
+#update May 7, 2020: calculate solar position to add a meaningful proxy for time of the day to the model (so, basically, the local time calculation was not necessary)
 
 #tutorials
 #https://www.r-bloggers.com/advanced-sab-r-metrics-parallelization-with-the-mgcv-package/
@@ -36,49 +36,114 @@ data <- df_lt %>%
   filter(between(lat,0,60)) %>% #filter for lat zone 0-60
   as.data.frame() %>% 
   st_as_sf(coords = c("lon","lat"), crs = wgs) %>% 
-  st_intersection(ocean)#filter out lakes
-  
+  st_intersection(ocean) %>% #filter out lakes
+  mutate(s_elev_angle = solarpos(st_coordinates(.), date_time, proj4string=CRS("+proj=longlat +datum=WGS84"))[,2]) %>% #calculate solar elevation angle
+  mutate(sun_elev = ifelse(s_elev_angle < -6, "night", #create a categorical variable for teh position of the sun
+                               ifelse(s_elev_angle > 40, "high", "low")),
+         month = month(date_time))
+ 
 save(data, file = "t_data_0_60.RData")
 
-#keep only daytime
+### STEP 2: data exploration #####
+#check to see if there are any monthly, hourly, latitudinal differences
+
+#running this on the entire dataset gives ram errors! so, just visualize the sample! lol
+
+X11(width = 15, height = 10);par(mfrow= c(2,2), oma = c(0,0,3,0))
+#for(i in c("sun_elev", "month","local_hour")){
+    
+  boxplot(sample$delta_t ~ sample$month, boxfill = "skyblue1", ylab = "delta_t")
+  boxplot(sample$delta_t ~ sample$sun_elev, boxfill = "wheat1", ylab = "delta_t")
+  boxplot(sample$delta_t ~ sample$local_hour, boxfill = "mediumblue", ylab = "delta_t")
+  plot(sample$delta_t ~ sample$lat , pch = 16, col = factor(sample$month), ylab = "delta_t")
+  
+
+mtext("40-yr averages at each point", side = 3, outer = T, cex = 1.3)
 
 
+### STEP 3: make the model #####
 
-#what about temporal filter? keep only daylight....
+#prep data
+data_df <- data %>% 
+  as("Spatial") %>% 
+  as.data.frame() %>% 
+  rename(lon = coords.x1,
+         lat = coords.x2)
 
-### STEP 2: make the model #####
+save(data_df, file = "t_data_df_0_60.RData")
 
 #try INLA also :p ... is probably more appropriate
 
-
-###step 4: modeling-testing-subset testing and training set #####
-
-#include in the model: delta T ~ s(yday)+ s(lon+lat) + local hour of day (as a random effect or just a factor?)
-#perhaps an interaction term for local hour and latitude to take into account seasonality?
+#include in the model: delta T ~ s(yday)+ s(lon,lat) + sun_elevation + random(year)
 
 #for modleing, use bam() instead of gam(), it breaks the data into chunks and has a lower memory footprint. also has the option to parallelize
+
 #work on a sample first
+sample <- data %>% 
+  st_crop(xmin = 120, xmax = 130, ymin = 25, ymax = 35) %>% 
+  as("Spatial") %>% 
+  as.data.frame() %>% 
+  rename(lon = coords.x1,
+         lat = coords.x2)
+
+model<-bam(delta_t ~ s(lon,lat) + s(yday) + factor(sun_elev) , data = sample)
+AIC(model)
+gam.check(model)
+
+#separate smooths for time of day
+model2 <- bam(delta_t ~ s(lon,lat) + s(yday,by = factor(sun_elev)) + factor(sun_elev) , data = sample)
+AIC(model2) 
+plot(model2)
+
+#separate smooths for time of day
+model3 <- bam(delta_t ~ s(lon,lat, by = as.numeric(sun_elev == "night")) +
+                s(lon,lat, by = as.numeric(sun_elev == "high")) +
+                s(lon,lat, by = as.numeric(sun_elev == "low")) + 
+                s(yday) + factor(sun_elev) , data = sample)
+AIC(model3)
+gam.check(model3)
 
 
-mycl<-makeCluster(detectCores()-1) #define workers :p
+b <- Sys.time()
+mycl <- makeCluster(9) 
 
-clusterExport(mycl, "sample") #define the variable that will be used within the function
+clusterExport(mycl, "data") #define the variable that will be used within the function
 
+clusterEvalQ(mycl, {
+  library(mgcv)
+  library(mapview)
+})
 
-model<-bam(delta_t ~ s(lon,lat) + s(yday) + factor(local_hour) , data=sample, cluster=mycl) #in the example: method="GCV.Cp",
-
+m1 <- bam(delta_t ~ s(lon,lat, by = as.numeric(sun_elev == "night")) +
+                s(lon,lat, by = as.numeric(sun_elev == "high")) +
+                s(lon,lat, by = as.numeric(sun_elev == "low")) + 
+                s(yday) + factor(sun_elev) , data = data, cluster = mycl)
 
 stopCluster(mycl)
 
 
-model<-bam(delta_t ~ s(lon,lat) + s(yday) + factor(local_hour) , data=sample)
+model<-bam(delta_t ~ s(lon,lat) + s(yday) + factor(local_hour) , data = sample)
 AIC(model) 
 plot(model)
 
-#separate smooths for hour
-model2<-bam(delta_t ~ s(lon,lat) + s(yday,by=factor(hour)) + factor(hour) , data=sample, cluster=mycl)
-AIC(model2) 
-plot(model2)
+
+
+model3<-bam(delta_t ~ s(lon,lat, by = factor(month)) + factor(sun_elev) , data = sample)
+AIC(model3) 
+plot(model3)
+
+gam.check(model3)
+
+plot_smooths(
+  model = model3,
+  series = lon,
+  comparison = factor(month)
+) +
+  theme(legend.position = "top")
+
+model4 <- gamm(delta_t ~ s(lon,lat) + s(yday,by = factor(hour)) + factor(hour),
+               random = list(year = ~1), data = sample)
+
 
 #compare models with Wald test
 anova(model, model2)
